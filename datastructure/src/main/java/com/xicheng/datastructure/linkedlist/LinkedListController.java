58c40280607c01b69ec5af2b8708d78dc8c49db8
liubin52 <liubin52@jd.com>
2018-12-26 21:24:28 +0800

    算法

---
+++ datastructure/src/main/java/com/xicheng/datastructure/linkedlist/LinkedListController.java
@@ -0,0 +1,20 @@
+package com.xicheng.datastructure.linkedlist;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+/**
+ * @author xichengxml
+ * @date 2018/12/26
+ * @description
+ */
+public class LinkedListController {
+
+    public String test01() {
+        List list = new LinkedList();
+        TreeSet treeMap = new TreeSet();
+        return null;
+    }
+}
---
+++ stream/src/main/java/com/xicheng/stream/socket/SocketTest.java
@@ -0,0 +1,14 @@
+package com.xicheng.stream.socket;
+
+import java.net.Socket;
+
+/**
+ * @author xichengxml
+ * @date 2018/12/26
+ * @description
+ */
+public class SocketTest {
+
+    public void test01() {
+    }
+}
