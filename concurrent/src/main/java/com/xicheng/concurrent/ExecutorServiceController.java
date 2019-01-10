6eb4fde62080c8f027011ece3850e1659fc1bfcc
liubin52 <liubin52@jd.com>
2019-01-10 19:35:19 +0800

    CyclicBarrier，倒排索引

--- concurrent/src/main/java/com/xicheng/concurrent/ExecutorServiceController.java
+++ concurrent/src/main/java/com/xicheng/concurrent/ExecutorServiceController.java
@@ -1,6 +1,7 @@
 package com.xicheng.concurrent;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.*;
@@ -9,18 +10,16 @@ import java.util.concurrent.*;
  * @author xichengxml
  * @date 2019/1/8 15:44
  */
-public class CyclicBarrierTest {
+public class ExecutorServiceController {
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        // 可以做AOP功能
-        CyclicBarrier cyclicBarrier = new CyclicBarrier(5, new MainTask());
         ExecutorService executorService = Executors.newFixedThreadPool(5);
-        InvokeTask task01 = new InvokeTask("task01", cyclicBarrier, 1000);
-        InvokeTask task02 = new InvokeTask("task02", cyclicBarrier, 2000);
-        InvokeTask task03 = new InvokeTask("task03", cyclicBarrier, 3000);
-        InvokeTask task04 = new InvokeTask("task04", cyclicBarrier, 4000);
-        InvokeTask task05 = new InvokeTask("task05", cyclicBarrier, 5000);
+        InvokeTask task01 = new InvokeTask("task01", 1000);
+        InvokeTask task02 = new InvokeTask("task02", 2000);
+        InvokeTask task03 = new InvokeTask("task03", 3000);
+        InvokeTask task04 = new InvokeTask("task04", 4000);
+        InvokeTask task05 = new InvokeTask("task05", 5000);
 
         List<InvokeTask> taskList = new ArrayList<>(5);
         Collections.addAll(taskList, task01, task02, task03, task04, task05);
@@ -28,46 +27,41 @@ public class CyclicBarrierTest {
         List<Future<ResponseMsg>> futures = new ArrayList<>();
         try {
             futures = executorService.invokeAll(taskList, 4, TimeUnit.SECONDS);
+            // futures = executorService.invokeAll(taskList);
         } catch (InterruptedException e) {
             System.out.println("do nothing");
         }
 
+        long middle = System.nanoTime();
         for (int i = 0; i < futures.size(); i++) {
             try {
                 System.out.println(futures.get(i).get(1, TimeUnit.NANOSECONDS));
             } catch (Exception e) {
+                futures.get(i).cancel(true);
                 e.printStackTrace();
             }
         }
-        System.out.println("耗时：" + (System.currentTimeMillis() - start));
+        System.out.println("取值耗时：" + (System.nanoTime() - middle));
+        System.out.println("运行耗时：" + (System.currentTimeMillis() - start));
+
     }
 }
 
-class MainTask extends Thread {
-
-    @Override
-    public void run() {
-        System.out.println("主任务执行");
-    }
-}
 class InvokeTask implements Callable<ResponseMsg> {
 
     private String name;
-    private CyclicBarrier cyclicBarrier;
     private int timeout;
 
     public InvokeTask() {
     }
 
-    public InvokeTask(String name, CyclicBarrier cyclicBarrier, int timeout) {
+    public InvokeTask(String name, int timeout) {
         this.name = name;
-        this.cyclicBarrier = cyclicBarrier;
         this.timeout = timeout;
     }
 
     @Override
     public ResponseMsg call() throws Exception {
-        cyclicBarrier.await(1, TimeUnit.SECONDS);
         Thread.sleep(timeout);
         System.out.println("Task " + name + " finished");
         return new ResponseMsg(name);
